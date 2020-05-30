d39f7bc4e2a5987ca7ec8e553daa72db10c5dc36
Shahzain Ahmed <59369881+ShahzainAhmed@users.noreply.github.com>
2020-05-31 01:07:55 +0500

    a

--- CheckNumbers.py
+++ CheckNumbers.py
@@ -2,6 +2,7 @@
 # Taking input from the user in floating point number.
 number = float(input("Enter a number:\n"))
 
+# Using an if statement to check if the number if positive. (If the number given by the user is greater than 0, then it is Positive.)
 if number > 0:
    print("Positive number")
    
