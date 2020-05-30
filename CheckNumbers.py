66c3e50303a70b35ffea7c9eb7263ab35bdaa402
Shahzain Ahmed <59369881+ShahzainAhmed@users.noreply.github.com>
2020-05-31 01:06:28 +0500

    s

--- CheckNumbers.py
+++ CheckNumbers.py
@@ -1,8 +1,20 @@
 # Declaring and initializing a variable.
+# Taking input from the user in floating point number.
 number = float(input("Enter a number:\n"))
+
 if number > 0:
    print("Positive number")
+   
 elif number == 0:
    print("Zero")
+   
 else:
    print("Negative number")
+
+   s
+   qqas
+   
+   aax
+   xz
+   sa
+   
